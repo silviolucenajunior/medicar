00cda335cec904b455a5749d178cebe872ec96d5
Silvio Lucena Junior <silvio.lucena.junior@intrapoint.com>
2020-05-30 00:36:08 +1200

    update repository

--- backend/modulos/consulta/models.py
+++ backend/modulos/consulta/models.py
@@ -1,6 +1,7 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.contrib.auth.models import User
-from datetime import datetime
+from datetime import datetime, date
 from modulos.medico.models import Medico
 from modulos.agenda.models import Agenda, Horario
 from .managers import ConsultaManager
@@ -8,6 +9,7 @@ from .managers import ConsultaManager
 class Consulta(models.Model):
   objects = ConsultaManager()
   data_agendamento = models.DateField(null = False, auto_now_add = True)
+  desmarcada = models.BooleanField(default = False)
   dia = models.ForeignKey(Agenda, null = False, on_delete = models.CASCADE)
   horario = models.ForeignKey(Horario, null = True, on_delete = models.SET_NULL)
   Medico = models.ForeignKey(Medico, null = False, on_delete = models.CASCADE)
@@ -17,3 +19,29 @@ class Consulta(models.Model):
     ordering = ['dia__data']
     verbose_name = "Consulta"
     verbose_name_plural = "Consultas"
+
+  def canDelete(self, usuario):
+    hoje = date.today()
+    if (self.usuario != usuario or self.dia.data < hoje):
+      return False
+
+    return True
+
+  def clean(self):
+    if (self._consultaNoPassado()):
+      raise ValidationError('A data encontrasse no passado.')
+    if (self._consultaIndisponivel()):
+      raise ValidationError('Data e hora indisponiveis.')
+
+  def _consultaNoPassado(self):
+    hoje = date.today()
+    hora_atual = datetime.now().time()
+    if ( self.dia.data < hoje or self.horario < hora_atual ):
+      return True
+
+    return False
+  
+  def _consultaIndisponivel(self):
+    consulta = Consulta.objects.find(dia = self.dia, horario = self.horario).first()
+
+    return consulta != None
--- backend/modulos/consulta/views.py
+++ backend/modulos/consulta/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, Response
 from rest_framework import permissions
 
 from .models import Consulta
@@ -8,3 +8,12 @@ class ConsultaViewSet(viewsets.ModelViewSet):
   queryset = Consulta.objects.all()
   serializer_class = ConsultaSerializer
   permission_classes = [permissions.IsAuthenticated]
+
+  def destroy(self, request, *args, **kwargs):
+      consulta = self.get_object()
+      if (consulta.canDelete(request.user)) :
+        consulta.desmarcada = True
+        consulta.save()
+
+        return Response()
+      return Response(data = 'Você não pode cancelar esta consulta.')

9c41c286292154b73792d77dc34051919e8e1373
Silvio Lucena Junior <silvio.lucena.junior@intrapoint.com>
2020-06-04 22:01:52 -0300

    update codebase

--- frontend/src/app/modulos/consulta/pages/listagem.page.ts
+++ frontend/src/app/modulos/consulta/pages/listagem.page.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './listagem.page.html',
+  styleUrls: ['./listagem.page.scss']
 })
 export class ConsultaListagemPage {
+  constructor (private router : Router ) {}
+
   novaConsulta() {
-    console.log("Cadastrar Nova Consuta");
+    this.router.navigate(['consultas/cadastro']);
   }
 }
